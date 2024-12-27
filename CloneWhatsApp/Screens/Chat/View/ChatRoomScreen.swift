b08281ff298b8487c982b4fba3192898f10e2f0a
Abdulsamet Gocmen <hellosamet09@icloud.com>
2024-12-27 13:30:43 +0300

    -feature: add ChatRoomScreen

---
+++ CloneWhatsApp/Screens/Chat/View/ChatRoomScreen.swift
@@ -0,0 +1,71 @@
+//
+//  ChatRoomScreen.swift
+//  CloneWhatsApp
+//
+//  Created by Abdulsamet Göçmen on 27.12.2024.
+//
+
+import SwiftUI
+
+struct ChatRoomScreen: View {
+    var body: some View {
+        ScrollView{
+            LazyVStack{
+                ForEach(0..<12) { _ in
+                    Text("PLACEHOLDER")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        .background(Color.gray.opacity(0.1))
+                }
+            }
+        }
+        .toolbarVisibility(.hidden, for: .tabBar)
+        .toolbar {
+            leadingNavItems()
+            trailingNavItems()
+        }
+        .safeAreaInset(edge: .bottom, content: {
+            TextInputArea()
+        })
+    }
+}
+
+extension ChatRoomScreen {
+    
+    @ToolbarContentBuilder
+    private func leadingNavItems() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            HStack{
+                Circle()
+                    .frame(width: 35, height: 35)
+                
+                Text("QaUser12")
+            }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    private func trailingNavItems() -> some ToolbarContent {
+        ToolbarItemGroup(placement: .topBarTrailing) {
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "video")
+            })
+            
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "phone")
+            })
+        }
+    }
+}
+
+#Preview {
+    NavigationStack{
+        ChatRoomScreen()
+    }
+}
---
+++ CloneWhatsApp/Screens/Chat/View/TextInputArea.swift
@@ -0,0 +1,78 @@
+//
+//  TextInputArea.swift
+//  CloneWhatsApp
+//
+//  Created by Abdulsamet Göçmen on 27.12.2024.
+//
+
+import SwiftUI
+
+struct TextInputArea: View {
+    @State private var text: String = ""
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 5){
+            imagePickerButton()
+                .padding(3)
+            audioRecorderButton()
+            messageTextField()
+            sendMessageButton()
+        }
+        .padding(.bottom)
+        .padding(.horizontal, 8)
+        .padding(.top, 10)
+        .background(Color.whatsAppWhite)
+    }
+    
+    private func messageTextField() -> some View {
+        TextField("", text: $text, axis: .vertical)
+            .padding(5)
+            .background(RoundedRectangle(cornerRadius: 20, style: .continuous).fill(.thinMaterial))
+            .overlay(textViewBorder())
+    }
+    
+    private func textViewBorder() -> some View {
+        RoundedRectangle(cornerRadius: 20, style: .continuous)
+            .stroke(Color(.systemGray5), lineWidth: 1)
+    }
+    
+    private func imagePickerButton() -> some View {
+        Button(action: {
+            
+        }, label: {
+            Image(systemName: "photo.on.rectangle")
+                .font(.system(size: 22))
+        })
+    }
+    
+    private func audioRecorderButton() -> some View {
+        Button(action: {
+            
+        }, label: {
+            Image(systemName: "mic.fill")
+                .fontWeight(.heavy)
+                .imageScale(.small)
+                .foregroundStyle(Color.white)
+                .padding(6)
+                .background(Color.blue)
+                .clipShape(Circle())
+                .padding(.horizontal, 3)
+        })
+    }
+    
+    private func sendMessageButton() -> some View {
+        Button(action: {
+            
+        }, label: {
+            Image(systemName: "arrow.up")
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.white)
+                .padding(6)
+                .background(Color.blue)
+                .clipShape(Circle())
+        })
+    }
+}
+
+#Preview {
+    TextInputArea()
+}
